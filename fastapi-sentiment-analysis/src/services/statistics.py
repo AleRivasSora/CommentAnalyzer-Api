e388b36651428b2a6eae74e4a86f8da5bb372bd0
fap-git <faip1399@gmail.com>
2025-01-01 17:46:35 -0400

    function about comments count

---
+++ fastapi-sentiment-analysis/src/services/statistics.py
@@ -0,0 +1,8 @@
+# 1. Tarea: escribir una funcion que reciba una lista de datos en string.
+# 2. voy a hacer un conteo de las palabras en los comentarios.
+# 3. suma general de cuales son las palabras mas utilizadas.
+
+# separar las palabras de la oracion y hacer una suma de general, 
+# cambiar la lista de comentarios a lista de palabras como matriz.
+
+comentarios = []
